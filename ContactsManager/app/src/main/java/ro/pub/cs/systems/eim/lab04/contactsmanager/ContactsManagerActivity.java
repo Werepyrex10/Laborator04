281633f18f9218ea13f9974cab3bb3912effc34c
Dimitriu Dragos-Cosmin <ddcdimix10@gmail.com>
2017-03-20 15:34:53 +0200

    Done lab 04

--- ContactsManager/app/src/main/java/ro/pub/cs/systems/eim/lab04/contactsmanager/ContactsManagerActivity.java
+++ ContactsManager/app/src/main/java/ro/pub/cs/systems/eim/lab04/contactsmanager/ContactsManagerActivity.java
@@ -1,14 +1,18 @@
 package ro.pub.cs.systems.eim.lab04.contactsmanager;
 
+import android.app.Activity;
 import android.content.ContentValues;
 import android.content.Intent;
+import android.nfc.Tag;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -18,6 +22,19 @@ public class ContactsManagerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts_manager);
+
+        Intent intent = getIntent();
+
+        if (intent != null) {
+            String phone = intent.getStringExtra("ro.pub.cs.systems.eim.lab04.contactsmanager.PHONE_NUMBER_KEY");
+            if (phone != null) {
+                EditText number = (EditText) findViewById(R.id.Number);
+                number.setText(phone);
+            }
+            else {
+                Toast.makeText(this, "Can't retrieve phone number", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
     public void onClickFields(View view) {
         RelativeLayout add = (RelativeLayout) findViewById(R.id.Additional);
@@ -80,10 +97,18 @@ public class ContactsManagerActivity extends AppCompatActivity {
         }
 
         intent.putParcelableArrayListExtra(ContactsContract.Intents.Insert.DATA, contactData);
-        startActivity(intent);
+        startActivityForResult(intent, 1);
     }
 
     public void onClickCancel(View view) {
+        setResult(Activity.RESULT_CANCELED, new Intent());
         finish();
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == 1) {
+            setResult(resultCode, new Intent());
+            finish();
+        }
     }
 }
